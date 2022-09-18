c09abfe149c50cfcd1b2063e863afcb59a0e7f5c
Alima07 <alex.lima_dev@outlook.com>
2022-09-18 20:37:42 -0300

    Ajustes no código para verificacao e correcao de rotas

--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -30,17 +30,28 @@ module.exports ={
 
     processingData: (req, res) => {
 
-         // Captura dos dados (NOME, E-MAIL E SENHA) enviados via Método Post através do req.body e salva em uma variavel
-         let dados = req.body;
+        // Verifica se o req.file é false (undefined) se sim executar o código abaixo
+        if (!req.file) {
 
-         // Inclusao do nome de usuario e senha da ultima posicao do array do banco de dados onde é guardado todos os usuarios e senhas
-         dadosUsuario.push(dados);
- 
-         // conversao dos dados em tipo Json, e atraves da funcao nativa fs o salvamento desses dados dentro da database em formato json
-         fs.writeFileSync('./database/dadosUsuario.json', JSON.stringify(dadosUsuario, null, 4));
- 
-         //Redirecionamento para a página de confirmacao do novo usuario e senha criada e exibicao de todos os usuarios e senhas para confirmacao
-         res.send('CADASTRO EFETUADO COM SUCESSO');
+            return res.send("Imagem Não Enviada!");
+            
+        } else {
+
+            // Captura dos dados (NOME, E-MAIL E SENHA) enviados via Método Post através do req.body e salva em uma variavel
+            let dados = req.body;
+
+            // Inclui do nome do arquivo de imagem do usuário na variavel Dados
+            dados.imagemUsuario = req.file.filename;
+
+            // Inclusao do nome de usuario e senha da ultima posicao do array do banco de dados onde é guardado todos os usuarios e senhas
+            dadosUsuario.push(dados);
+    
+            // conversao dos dados em tipo Json, e atraves da funcao nativa fs o salvamento desses dados dentro da database em formato json
+            fs.writeFileSync('./database/dadosUsuario.json', JSON.stringify(dadosUsuario, null, 4));
+    
+            //Redirecionamento para a página de confirmacao do novo usuario e senha criada e exibicao de todos os usuarios e senhas para confirmacao
+            res.send('CADASTRO EFETUADO COM SUCESSO');
+        }
 
     },
 
--- routers/RouterIndex.js
+++ routers/RouterIndex.js
@@ -4,6 +4,17 @@ const express = require('express');
 // Requisao do Middleware para captura da data e hora de acesso da rota home
 const registroRotaHome = require('../middlewares/logRotaHome');
 
+// Requizicao do Método Multer para trabalhar com processamento de arquivos do lado do servidor
+const multer = require('multer');
+
+// Requisicao do Middleware multerDiskStorage
+const multerDiskStorage = require("../middlewares/multerDiskStorage");
+
+// Funcao que ira receber os dados e passar para o storage fazer o trabalho de destination e filename
+const upload = multer({ storage: multerDiskStorage});
+
+//-------------------BLOCO EXPRESS-VALIDATOR------------------//
+
 // Requisicao do express-validator destruturacao do body
 const { body } = require('express-validator');
 
@@ -13,7 +24,7 @@ const validacoes = [
     body("email").notEmpty().isEmail(),
     body("senha").notEmpty().isString()
 ];
-
+//------------------------------------------------------------//
 
 // Confiuracao do modulo express para chamar a função como router
 const router = express.Router();
@@ -24,25 +35,26 @@ const IndexController = require('../controllers/IndexController');
 // Rota para página HOME
 // Segundo parametro para chamar o middleware de rota registro Home
 router.get('/', registroRotaHome, IndexController.index);
-// Rota para página HOME
+
+// Rota para página PRODUTOS
 router.get('/produtos', IndexController.produtos);
-// Rota para página HOME
+
+// Rota para página CONTATO
 router.get('/contato', IndexController.contato);
 
-//-----------------------------------------------------//
-// Rota para página HOME
+// Rota para página MINHA CONTA
 router.get('/minhaConta', IndexController.minhaConta);
 
-
-router.post('/minhaConta', validacoes, IndexController.processingData);
-
-
-
-
 //-----------------------------------------------------//
 
-// Rota para página HOME
+// Rota para página CADASTRO
 router.get('/cadastro', IndexController.cadastro);
+
+// No Segundo parametro efetuamos as validacoes dos campo de formulário
+// No terceiro parametro temos o middleware de rota para chamar a funcao multer onde indicamos o caminho onde sera salvo o arquivo e nome desse arquivo (IMAGEM USUARIO)
+router.post('/cadastro', validacoes , upload.single('imagemUsuario'), IndexController.processingData);
+
+//-----------------------------------------------------//
 
 // Exportando o index router para ser usado no Indexcontroller
 module.exports = router; 
