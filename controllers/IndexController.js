3b6aac291a96e214aa0e8c2491515f7719280d14
Alima07 <alex.lima_dev@outlook.com>
2022-09-18 19:49:48 -0300

    Instalacao da biblioteca express-validator, criacao do array de validacoes e inclusao como parametro na rota post, captura dos dados de cadastro e logica para guardar os dados capturados em um arquivo.json, efetuado testes.

--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -1,3 +1,9 @@
+// Requisicao do banco de Dados
+const dadosUsuario = require("../database/dadosUsuario.json")
+
+// Requisicao do modulo nativo file system
+const fs = require('fs');
+
 // Objeto Literal ja pronto para exportacao de todos os controllers
 module.exports ={
 
@@ -7,17 +13,37 @@ module.exports ={
     index: (req, res) =>{
         res.render("index.ejs");
     },
+
     produtos: (req, res) =>{
         res.render("produtos.ejs");
     },
+
     contato: (req, res) =>{
         res.render("contato.ejs");
 
     },
+    
     minhaConta: (req, res) =>{
         res.render("minhaConta.ejs");
 
     },
+
+    processingData: (req, res) => {
+
+         // Captura dos dados (NOME, E-MAIL E SENHA) enviados via Método Post através do req.body e salva em uma variavel
+         let dados = req.body;
+
+         // Inclusao do nome de usuario e senha da ultima posicao do array do banco de dados onde é guardado todos os usuarios e senhas
+         dadosUsuario.push(dados);
+ 
+         // conversao dos dados em tipo Json, e atraves da funcao nativa fs o salvamento desses dados dentro da database em formato json
+         fs.writeFileSync('./database/dadosUsuario.json', JSON.stringify(dadosUsuario, null, 4));
+ 
+         //Redirecionamento para a página de confirmacao do novo usuario e senha criada e exibicao de todos os usuarios e senhas para confirmacao
+         res.send('CADASTRO EFETUADO COM SUCESSO');
+
+    },
+
     cadastro: (req, res) =>{
         res.render("cadastro.ejs");
 
--- routers/RouterIndex.js
+++ routers/RouterIndex.js
@@ -1,7 +1,19 @@
 // Requizicao do modulo NPM Express
 const express = require('express');
+
 // Requisao do Middleware para captura da data e hora de acesso da rota home
 const registroRotaHome = require('../middlewares/logRotaHome');
+
+// Requisicao do express-validator destruturacao do body
+const { body } = require('express-validator');
+
+// Variavel do tipo array para validacao de cada campo do body como funcao do express-validator
+const validacoes = [
+    body("nomeCompleto").notEmpty().isString(),
+    body("email").notEmpty().isEmail(),
+    body("senha").notEmpty().isString()
+];
+
 
 // Confiuracao do modulo express para chamar a função como router
 const router = express.Router();
@@ -21,6 +33,8 @@ router.get('/contato', IndexController.contato);
 // Rota para página HOME
 router.get('/minhaConta', IndexController.minhaConta);
 
+
+router.post('/minhaConta', validacoes, IndexController.processingData);
 
 
 
